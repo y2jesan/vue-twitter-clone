9466ba7ce282f6c7328ed1a7b9ce8f2832768c45
y2jesan <ymjesanlemon@gmail.com>
2020-10-28 16:59:57 +0600

    basic data and method

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome 2 Vue Jesan"/>
+  <div id="appC">
+    @{{ user.userName }} {{ getFullName }}
+    <br/><br/>
+    Followers: <strong>{{ followers }}</strong>
+    <br/><br/>
+    <button @click="FollowUser">Follow</button>
+  </div>  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      isLoading: false,
+      followers: 0,
+      user: {
+        id: 1,
+        userName: "ymjess",
+        firstName: "Yeasin Mahmud",
+        lastName: "Jesan",
+        email: "ymjesanlemon@gmail.com",
+        isAdmin: true,
+      },
+    };
+  },
+  computed: {
+    getFullName(){
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  methods: {
+    FollowUser(){
+      this.followers++;
+    }
+  },
+  mounted(){
+    this.followers = 5;
   }
-}
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
 }
 </style>
