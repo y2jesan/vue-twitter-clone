4225f576d0d597a994ea3dad1ac4305f1c2bd9bc
y2jesan <ymjesanlemon@gmail.com>
2020-10-29 12:33:53 +0600

    api call

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,11 @@
     Followers: <strong>{{ followers }}</strong>
     <br/><br/>
     <button @click="FollowUser">Follow</button>
+    <div class="m-t-10">
+    <ul class="p-0">
+        <dd class="p-0" v-for="product in products" :key="product._id">{{product.title}}</dd>
+    </ul>
+    </div>
   </div>  
 </template>
 
@@ -23,6 +28,7 @@ export default {
         email: "ymjesanlemon@gmail.com",
         isAdmin: true,
       },
+      products: {}
     };
   },
   computed: {
@@ -30,13 +36,27 @@ export default {
       return `${this.user.firstName} ${this.user.lastName}`
     }
   },
+  watch:{
+    followers(newCount,oldCount){
+      console.log(newCount,oldCount);
+    },
+    products(){
+      this.$forceUpdate();
+      console.log(this.products);
+    }
+  },
   methods: {
     FollowUser(){
       this.followers++;
+    },
+    async getAllProducts(){
+      await fetch("http://localhost:3000/posts/getAllProducts").then(response => response.json())
+  .then(data => {console.log(data);this.products = data;});
     }
   },
   mounted(){
     this.followers = 5;
+    this.getAllProducts();
   }
 };
 </script>
@@ -49,5 +69,12 @@ export default {
   margin-top: 60px;
   display: flex;
   flex-direction: column;
+}
+.p-0{
+  padding: 0%;
+  margin: 0%;
+}
+.m-t-10{
+  margin-top: 10px;
 }
 </style>
